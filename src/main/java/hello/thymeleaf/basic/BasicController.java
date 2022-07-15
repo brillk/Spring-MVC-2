53a26272f6ecb9902f9bf3597ee3644a699f9257
KJH <stonehead334@gmail.com>
2022-07-15 15:48:08 +0900

    블록

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -113,6 +113,14 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+       addUsers(model);
+        return "basic/block";
+    }
+
+
     private void addUsers(Model model) {
         ArrayList<Object> list = new ArrayList<>();
         list.add(new User("UserA", 10));
