a34317031797a2617157f017dffe6248a41e5ecc
KJH <stonehead334@gmail.com>
2022-07-15 16:51:01 +0900

    템플릿 레이아웃2

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -23,4 +23,9 @@ public class TemplateController {
      * 정보들을 한 곳에 모아두고, 공통으로 사용하지만, 각 페이지마다 필요한 정보를 더 추가해서 사용하고
      * 싶다면 다음과 같이 사용하면 된다.
      */
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
 }
