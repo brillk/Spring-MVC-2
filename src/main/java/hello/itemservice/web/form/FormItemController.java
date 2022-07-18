91df0cf8b95cfa7acbcaee398a47d254c53e62ee
KJH <stonehead334@gmail.com>
2022-07-18 09:35:59 +0900

    체크 박스 - 단일 1

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,10 +41,25 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen()); // true
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/form/items/{itemId}";
+
+        //실행 로그
+        //FormItemController : item.open=true //체크 박스를 선택하는 경우
+        //FormItemController : item.open=false //체크 박스를 선택하지 않는 경우
+        //체크 박스 체크
+        //open=on&_open=on
+        //체크 박스를 체크하면 스프링 MVC가 open 에 값이 있는 것을 확인하고 사용한다. 이때 _open 은
+        //무시한다.
+        //체크 박스 미체크
+        //_open=on
+        //체크 박스를 체크하지 않으면 스프링 MVC가 _open 만 있는 것을 확인하고, open 의 값이 체크되지
+        //않았다고 인식한다.
+        //이 경우 서버에서 Boolean 타입을 찍어보면 결과가 null 이 아니라 false 인 것을 확인할 수 있다.
+        //log.info("item.open={}", item.getOpen());
     }
 
     @GetMapping("/{itemId}/edit")
