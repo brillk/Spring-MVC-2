ef9e2f832701798cf19493951d571db667518d5c
KIM <stonehead334@gmail.com>
2022-07-22 16:35:28 +0900

    Servlet Exception Process - Interceptor

--- src/main/java/hello/exception/filter/LogFilter.java
+++ src/main/java/hello/exception/filter/LogFilter.java
@@ -14,6 +14,7 @@ public class LogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
+
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
         String uuid = UUID.randomUUID().toString();
